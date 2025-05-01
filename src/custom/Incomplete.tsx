991a8fa5d82d86e6fb5903876ea62646a3b91cd8
Blake Bishop <blakesterbishop@gmail.com>
2025-05-01 15:09:59 -0700

    Version release for icons test of rating star and incomplete

--- src/custom/Incomplete.tsx
+++ src/custom/Incomplete.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import createSvgIcon from "../utils/createSvgIcon";
 
 export default createSvgIcon(
-  <>
-    {/* Reduced strokeWidth for background circle */}
-    <circle cx="12" cy="12" r="8" stroke="#EBECF0" strokeWidth="1.5" />
-    <mask id="a" fill="#fff">
-      <path d="M11.86 3.953c-.02-1.078.847-1.986 1.908-1.796A10 10 0 1 1 2.225 14.11c-.227-1.053.65-1.95 1.728-1.97 1.077-.018 1.937.862 2.294 1.88a6.097 6.097 0 1 0 7.57-7.84c-1.029-.321-1.939-1.15-1.957-2.227Z" />
-    </mask>
-    <path
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      opacity={0.12}
+      cx={12}
+      cy={12}
+      r={10}
       stroke="#253858"
-      // Reduced strokeWidth for main path
-      strokeWidth="2"
-      d="M11.86 3.953c-.02-1.078.847-1.986 1.908-1.796A10 10 0 1 1 2.225 14.11c-.227-1.053.65-1.95 1.728-1.97 1.077-.018 1.937.862 2.294 1.88a6.097 6.097 0 1 0 7.57-7.84c-1.029-.321-1.939-1.15-1.957-2.227Z"
-      mask="url(#a)"
+      strokeWidth={2}
     />
-  </>,
+    <path
+      d="M11.825 1.993c-.01-.547.428-1.004.974-.964A11 11 0 1 1 1.064 13.181c-.06-.544.382-.997.93-1.006.547-.01.993.428 1.062.97a9.017 9.017 0 1 0 9.777-10.123c-.545-.05-.998-.481-1.008-1.029"
+      fill="#253858"
+    />
+  </svg>,
   "Incomplete",
 );
